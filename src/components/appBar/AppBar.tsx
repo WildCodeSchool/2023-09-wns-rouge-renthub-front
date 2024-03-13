ec861420d09702496c6eb48c28cf3954d57ee90b
Wardzek <tom.tournier.tt@gmail.com>
2024-03-13 11:55:10 +0100

    add appBar

--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -1,307 +1,83 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { useState } from 'react';
 import Link from 'next/link';
-import IconButton from '@mui/material/IconButton';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { useMutation, useQuery } from '@apollo/client';
-import { mutationSignOut, queryMeContext } from '../graphql/Users';
-import { UserContextTypes } from '@/types/UserTypes';
-import {
-  AppBar,
-  Avatar,
-  Box,
-  Button,
-  Container,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from '@mui/material';
-import CookieIcon from '@mui/icons-material/Cookie';
-import EditNoteIcon from '@mui/icons-material/EditNote';
-import SearchIcon from '@mui/icons-material/Search';
-import ContactSupportIcon from '@mui/icons-material/ContactSupport';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LoginIcon from '@mui/icons-material/Login';
-import MenuIcon from '@mui/icons-material/Menu';
-import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { PATH_IMAGE } from '@/api/configApi';
+import Image from 'next/image';
+import { OrangeBtnBlueHover } from '@/styles/MuiButtons';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 
-const buttonStyles = {
-  color: 'white',
-  '& .MuiButton-startIcon': {
-    marginRight: '-4px',
-  },
-};
-
-export default function Header(): React.ReactNode {
+function ResponsiveAppBar() {
+  const [searchWord, setSearchWord] = useState('');
   const router = useRouter();
-  // User connected ?
-  const { data, error } = useQuery<{ item: UserContextTypes }>(queryMeContext);
-  const [userContext, setUserContext] = useState<UserContextTypes>(null);
-  const [userConnected, setUserConnected] = useState<Boolean>(false);
 
-  useEffect(() => {
-    if (error) {
-      setUserContext(null);
-      setUserConnected(false);
-    }
-    if (data?.item) {
-      setUserContext(data.item);
-      setUserConnected(true);
-    }
-  }, [data, error]);
-
-  // Signout
-  const [doSignout] = useMutation(mutationSignOut, {
-    onCompleted: () => {
-      setUserContext(null);
-      setAnchorElUser(null);
-      setUserConnected(false);
-      router.replace(`/connexion`);
-    },
-    refetchQueries: [{ query: queryMeContext }],
-  });
-  async function logout() {
-    doSignout();
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    router.push(`/?searchWord=${searchWord.trim()}`);
   }
 
-  // Open / Close menu
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
+  const [bulleVisible, setBulleVisible] = useState(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#343a40' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters className="header">
-          <Box
-            className="header_title"
-            sx={{
-              display: { xs: 'none', md: 'flex' },
-            }}
-          >
-            <Link href="/">
-              <CookieIcon className="header_title_logo" />
-            </Link>
-            <Link href="/" className="header_title_name">
-              RENTHUB
-            </Link>
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              sx={{
-                color: '#e89116',
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              <MenuItem
-                onClick={() => {
-                  handleCloseNavMenu();
-                  router.replace(`/annonces/new`);
-                }}
-              >
-                <Typography textAlign="center"> Déposer une annonce</Typography>
-              </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  handleCloseNavMenu();
-                  router.replace(`/recherche`);
-                }}
-              >
-                <Typography textAlign="center"> Recherche</Typography>
-              </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  handleCloseNavMenu();
-                  router.replace(`/compte`);
-                }}
-              >
-                <Typography textAlign="center"> Compte</Typography>
-              </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  handleCloseNavMenu();
-                  router.replace(`/contact`);
-                }}
-              >
-                <Typography textAlign="center"> Contact</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <CookieIcon
-            sx={{
-              display: { xs: 'flex', md: 'none' },
-              mr: 1,
-              color: '#e89116',
-            }}
+    <AppBar className="header" position="static">
+      <div className="main-menu">
+        <Link href="/" className="button logo link-button">
+          {/* <span className="desktop-long-label">RENTHUB</span> */}
+          <Image
+            src={'/images/logo.png'}
+            alt="logo"
+            width={201}
+            height={38.74}
           />
-          <Typography
-            variant="h4"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontWeight: 700,
-              letterSpacing: '.2rem',
-              color: '#e89116',
-              textDecoration: 'none',
-            }}
-          >
-            RENTHUB
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <Button
-              className="header_link_button"
-              startIcon={<EditNoteIcon fontSize="large" />}
-              onClick={() => {
-                handleCloseNavMenu();
-                router.replace(`/annonces/new`);
-              }}
-              sx={buttonStyles}
+        </Link>
+        <OrangeBtnBlueHover
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          Nos matériels <KeyboardArrowDownIcon />
+        </OrangeBtnBlueHover>
+        <form className="text-field-with-button" onSubmit={onSubmit}>
+          <input
+            className="text-field main-search-field"
+            type="search"
+            value={searchWord}
+            onChange={e => setSearchWord(e.target.value)}
+            placeholder="Rechercher par matériel"
+          />
+          <button className="button button-primary" type="submit">
+            <svg
+              aria-hidden="true"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="-50 -50 530 550"
+              xmlSpace="preserve"
+              className="styled__BaseIcon-sc-1jsm4qr-0 llmHhT"
             >
-              Déposer une annonce
-            </Button>
-            <Button
-              className="header_link_button"
-              startIcon={<SearchIcon />}
-              onClick={() => {
-                handleCloseNavMenu();
-                router.replace(`/recherche`);
-              }}
-              sx={buttonStyles}
-            >
-              Recherche
-            </Button>
-            <Button
-              className="header_link_button"
-              startIcon={<AccountCircleIcon />}
-              onClick={() => {
-                handleCloseNavMenu();
-                router.replace(`/compte`);
-              }}
-              sx={buttonStyles}
-            >
-              Compte
-            </Button>
-            <Button
-              className="header_link_button"
-              startIcon={<ContactSupportIcon />}
-              onClick={() => {
-                handleCloseNavMenu();
-                router.replace(`/contact`);
-              }}
-              sx={buttonStyles}
-            >
-              contact
-            </Button>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Ouvrir le menu du profil">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar
-                  alt="User avatar"
-                  src={
-                    userConnected && userContext.picture
-                      ? `${PATH_IMAGE}/pictures/${userContext.picture}`
-                      : `${PATH_IMAGE}/default/avatar.webp`
-                  }
-                />
-              </IconButton>
-            </Tooltip>
-
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {userConnected ? (
-                <Box>
-                  <MenuItem
-                    onClick={() => {
-                      handleCloseUserMenu();
-                      router.replace(`/compte`);
-                    }}
-                  >
-                    <AccountCircleIcon />
-                    <Typography textAlign="center">Mon compte</Typography>
-                  </MenuItem>
-                  <MenuItem onClick={logout}>
-                    <ExitToAppIcon />
-                    <Typography textAlign="center">Se déconnecter</Typography>
-                  </MenuItem>
-                </Box>
-              ) : (
-                <MenuItem
-                  onClick={() => {
-                    handleCloseUserMenu();
-                    router.replace(`/connexion`);
-                  }}
-                >
-                  <LoginIcon />
-                  <Typography textAlign="center">Connexion</Typography>
-                </MenuItem>
-              )}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
+              <path d="m464.524 412.846-97.929-97.925c23.6-34.068 35.406-72.047 35.406-113.917 0-27.218-5.284-53.249-15.852-78.087-10.561-24.842-24.838-46.254-42.825-64.241-17.987-17.987-39.396-32.264-64.233-42.826C254.246 5.285 228.217.003 200.999.003c-27.216 0-53.247 5.282-78.085 15.847C98.072 26.412 76.66 40.689 58.673 58.676c-17.989 17.987-32.264 39.403-42.827 64.241C5.282 147.758 0 173.786 0 201.004c0 27.216 5.282 53.238 15.846 78.083 10.562 24.838 24.838 46.247 42.827 64.234 17.987 17.993 39.403 32.264 64.241 42.832 24.841 10.563 50.869 15.844 78.085 15.844 41.879 0 79.852-11.807 113.922-35.405l97.929 97.641c6.852 7.231 15.406 10.849 25.693 10.849 9.897 0 18.467-3.617 25.694-10.849 7.23-7.23 10.848-15.796 10.848-25.693.003-10.082-3.518-18.651-10.561-25.694zM291.363 291.358c-25.029 25.033-55.148 37.549-90.364 37.549-35.21 0-65.329-12.519-90.36-37.549-25.031-25.029-37.546-55.144-37.546-90.36 0-35.21 12.518-65.334 37.546-90.36 25.026-25.032 55.15-37.546 90.36-37.546 35.212 0 65.331 12.519 90.364 37.546 25.033 25.026 37.548 55.15 37.548 90.36 0 35.216-12.519 65.331-37.548 90.36z"></path>
+            </svg>
+          </button>
+        </form>
+        <Link href="/" className="button link-button">
+          <HomeOutlinedIcon fontSize="small"></HomeOutlinedIcon>
+          <span className="desktop-long-label">Nos agences</span>
+        </Link>
+        <Link href="/" className="button link-button">
+          <PersonOutlineOutlinedIcon fontSize="small"></PersonOutlineOutlinedIcon>
+          <span className="desktop-long-label">Se connecter</span>
+        </Link>
+        <Link href="/" className="button link-button">
+          <ShoppingBasketOutlinedIcon fontSize="small"></ShoppingBasketOutlinedIcon>
+          <span className="desktop-long-label">Mon panier</span>
+        </Link>
+      </div>
     </AppBar>
   );
 }
+export default ResponsiveAppBar;
