cd093c6f22175bd85f71f31249c55db43cc8d63b
lukasz <zed11temp@gmail.com>
2024-04-01 04:02:22 +0200

    homePage section Promotions finished

--- src/styles/MuiStyled.ts
+++ src/styles/MuiStyled.ts
@@ -1,4 +1,7 @@
 import { styled } from "@mui/material";
+import { VariablesColors } from "./Variables.colors";
+
+const { color5 } = new VariablesColors();
 
 export const DownloadInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -10,4 +13,15 @@ export const DownloadInput = styled("input")({
   left: 0,
   whiteSpace: "nowrap",
   width: 1,
+});
+
+export const PromoTag = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: color5,
+  width: "fit-content",
+  padding: "0.2rem 1.5rem",
+  borderRadius: "20px",
+  rotate: "-5deg",
 });
