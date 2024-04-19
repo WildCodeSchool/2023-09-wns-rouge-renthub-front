d34168692ab70c79a982c22fd850e284517782bb
HugoDvdTApp <hugo@toolapp.fr>
2024-04-19 11:21:54 +0200

    adjust nav done

--- src/components/appBar/Navbar.tsx
+++ src/components/appBar/Navbar.tsx
@@ -1,55 +1,35 @@
 import {
   AppBar,
-  Avatar,
   Box,
   Button,
   Container,
   FormControl,
   IconButton,
-  Input,
   InputAdornment,
   InputLabel,
-  Link,
-  Menu,
-  MenuItem,
   OutlinedInput,
   Toolbar,
-  Tooltip,
-  Typography,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import AdbIcon from "@mui/icons-material/Adb";
-import Image from "next/image";
+import ExpandCircleDownOutlinedIcon from "@mui/icons-material/ExpandCircleDownOutlined";
 import * as React from "react";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { OrangeBtnBlueHover } from "@/styles/MuiButtons";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import ResponsiveMenu from "./ResponsiveMenu";
+import { VariablesColors } from "@/styles/Variables.colors";
 
 function Navbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null,
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null,
-  );
+  const [globalFilterValue, setGlobalFilterValue] =
+    React.useState<string>(""); /* global filter for search field */
+  const router = useRouter();
+  const { orangeColor } = new VariablesColors();
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const pages = ["Nos agences", "Se connecter", "Mon panier"];
-  const settings = ["Profile", "Account", "Dashboard", "Logout"];
+  /* Menu item, redirect to the well path */
+  const pages = [
+    { title: "Nos agences", path: "/agences" },
+    { title: "Se connecter", path: "/signin" },
+    { title: "Mon panier", path: "/cart" },
+  ];
 
   return (
     <AppBar position="sticky" sx={{ backgroundColor: "#fff" }}>
@@ -61,10 +41,14 @@ function Navbar() {
             sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
           >
             <Image
-              src={"./public/images/logo.svg"}
+              src={"/images/renthub-logo.png"}
               alt="logo"
               width={201}
               height={38.74}
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                router.push("/");
+              }}
             />
             <Button
               sx={{
@@ -79,17 +63,32 @@ function Navbar() {
               Nos matériels
             </Button>
           </Box>
+          {/* Responsive */}
+          {/* Display favicon logo on mobile */}
           <Box
             sx={{ display: { xs: "flex", md: "none" }, alignItems: "center" }}
           >
             <Image
-              src={"./public/images/favicon.svg"}
-              alt="favicon"
-              width={30}
-              height={30}
+              src="/images/favicon.png"
+              alt="logo"
+              width={40}
+              height={40}
             />
+            {/* @TODO: set modal in responsive menu */}
+            <IconButton
+              aria-label="materiels"
+              size="large"
+              sx={{
+                color: orangeColor,
+              }}
+              onClick={() => {
+                // displaying the modal...
+              }}
+            >
+              <ExpandCircleDownOutlinedIcon fontSize="inherit" />
+            </IconButton>
           </Box>
-          <FormControl sx={{ mx: "auto", width: "25ch" }} variant="outlined">
+          <FormControl sx={{ mx: "auto", width: "30ch" }} variant="outlined">
             <InputLabel htmlFor="search" size="small">
               Rechercher
             </InputLabel>
@@ -97,6 +96,10 @@ function Navbar() {
               id="search"
               type="text"
               size="small"
+              onClick={(e) => {
+                const inputElement = e.target as HTMLInputElement;
+                setGlobalFilterValue(inputElement.value);
+              }}
               endAdornment={
                 <InputAdornment position="end">
                   <IconButton aria-label="search">
@@ -115,49 +118,20 @@ function Navbar() {
             }}
             gap={2}
           >
-            {pages.map((page) => (
+            {pages.map((page, idx) => (
               <Button
-                key={page}
-                onClick={handleCloseNavMenu}
+                key={idx}
+                onClick={() => {
+                  router.push(page.path);
+                }}
                 sx={{ my: 2, color: "black", display: "block" }}
               >
-                {page}
+                {page.title}
               </Button>
             ))}
           </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          {/* Responsive Component usefull for displaying menu item {page} with menu icon icon  */}
+          <ResponsiveMenu pages={pages} router={router} />
         </Toolbar>
       </Container>
     </AppBar>
