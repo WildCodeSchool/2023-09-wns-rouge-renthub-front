820e207453c14271ac18581a77b33e5287b653f8
Mega Krash <jscattolini@gmail.com>
2024-06-11 21:32:44 +0200

    :construction: createCatPage & :truck:

--- src/pages/renthub-backoffice/categories/index.tsx
+++ src/pages/renthub-backoffice/categories/index.tsx
@@ -1,5 +1,5 @@
 import AdminProtection from "@/components/backoffice/AdminProtection";
-import BackOfficeCategoriesList from "@/components/backoffice/categories/BackOfficeCategoriesList";
+import BackOfficeCategoriesList from "@/components/backoffice/categories/list/BackOfficeCategoriesList";
 const BackOfficeCategories = (): React.ReactNode => {
   return (
     <>
