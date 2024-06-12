a8075ccf40ec9a5f4643a57f8aa03581ec6efa44
Mega Krash <jscattolini@gmail.com>
2024-06-12 09:23:45 +0200

    :recycle: & :building_construction: _App.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,130 +1,27 @@
 import "@/styles/index.scss";
-
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-  useQuery,
-} from "@apollo/client";
+import React from "react";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { UserProvider } from "@/context/UserContext";
-import { queryMeContext } from "@/components/graphql/Users";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { UserContextTypes } from "@/types/UserTypes";
-import { API_URL } from "@/api/configApi";
+import { client } from "@/config/apollo-client";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/appBar/Navbar";
 import BackOfficeLayout from "@/components/backoffice/layout/BackOfficeLayout";
-import React from "react";
 import LoadingApp from "@/styles/LoadingApp";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: ["Poppins", "sans-serif"].join(","),
-    h1: {
-      fontFamily: ["Poppins-Medium", "sans-serif"].join(","),
-    },
-    h2: {
-      fontFamily: ["Poppins-Medium", "sans-serif"].join(","),
-    },
-    h3: {
-      fontFamily: ["Poppins-Medium", "sans-serif"].join(","),
-    },
-    h4: {
-      fontFamily: ["Poppins-Medium", "sans-serif"].join(","),
-    },
-    h5: {
-      fontFamily: ["Poppins-Medium", "sans-serif"].join(","),
-    },
-  },
-
-  palette: {
-    mode: "light",
-    background: {
-      default: "#FFFFFF",
-    },
-    primary: {
-      main: "#FF8E3C",
-      light: "#FFB648",
-      dark: "#e89116",
-    },
-    secondary: {
-      main: "#343a40",
-      light: "#5C6166",
-      dark: "#24282C",
-    },
-  },
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root: {
-          backgroundColor: "#F8F8F8",
-        },
-      },
-    },
-    MuiPopover: {
-      styleOverrides: {
-        paper: {
-          backgroundColor: "white",
-          borderRadius: "1rem",
-        },
-      },
-    },
-
-    MuiLink: {
-      styleOverrides: {
-        root: {
-          color: "#FF8E3C",
-        },
-      },
-    },
-  },
-});
-
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: API_URL || "/api",
-    credentials: "include",
-  }),
-  cache: new InMemoryCache(),
-});
+import { useAuth } from "@/config/userAuth";
+import theme from "@/config/themeMui";
 
 const privatePages = ["/compte"];
 
-function AuthProvider({ children }: { children: React.ReactNode }) {
-  const { loading, error, refetch } = useQuery<{
-    item: UserContextTypes;
-  }>(queryMeContext);
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = () => {
-      if (privatePages.includes(router.pathname)) {
-        refetch();
-      }
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router, refetch]);
-
-  useEffect(() => {
-    if (privatePages.includes(router.pathname) && error) {
-      router.push("/signin");
-    }
-  }, [router, error]);
-
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { loading } = useAuth(privatePages);
   if (loading) return <LoadingApp />;
-
   return children;
-}
+};
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
