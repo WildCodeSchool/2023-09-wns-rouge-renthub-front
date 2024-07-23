6aeac9efc5585676b101d4b9ed25ae8836f9e451
lukasz <zed11temp@gmail.com>
2024-07-23 15:49:50 +0200

    cart-order component. + availability inside a cart. Each product has its own

--- src/components/cart/CartProducts.tsx
+++ src/components/cart/CartProducts.tsx
@@ -1,14 +1,21 @@
-import { Box, Button, Link, TextField, Typography } from "@mui/material";
-import { FaOpencart } from "react-icons/fa6";
-import { GET_CART } from "@/graphql/cart/cart";
-import { useQuery } from "@apollo/client";
+import { Box, Typography } from "@mui/material";
 import { VariablesColors } from "@/styles/Variables.colors";
-import { Cart } from "@/types/Cart";
 import { format } from "date-fns";
+import { useCartContext } from "@/context/CartContext";
+import { useQuery } from "@apollo/client";
+import { GET_COUNT_STOCKS_AVAILABLE_BY_DATES_PRODUCTREFERENCEID } from "@/graphql/stocks/queryStocks";
+import ProductCartAvailability from "./ProductCartAvailability";
+import { ProductCart } from "@/types/ProductCart";
 
 export default function CartProducts() {
-  const { data, loading, error } = useQuery<{ item: Cart }>(GET_CART);
+  const { cart, refetchCartContext } = useCartContext();
+  refetchCartContext();
   const { lightGreyColor } = new VariablesColors();
+
+  const { data: countStockAvaiable, refetch } = useQuery<{ count: number }>(
+    GET_COUNT_STOCKS_AVAILABLE_BY_DATES_PRODUCTREFERENCEID,
+  );
+
   return (
     <>
       <Box>
@@ -30,7 +37,7 @@ export default function CartProducts() {
         </Box>
       </Box>
       <Box sx={{ display: "flex", gap: 1, flexDirection: "column" }}>
-        {data?.item.productCarts.map((productCart) => {
+        {cart?.productCarts.map((productCart: ProductCart) => {
           return (
             <Box
               key={productCart.id}
@@ -38,22 +45,29 @@ export default function CartProducts() {
                 bgcolor: lightGreyColor,
                 borderRadius: "10px",
                 padding: "1rem",
+                display: "flex",
+                justifyContent: "space-between", // Align items to the left and right
+                alignItems: "center", // Center items vertically
               }}
             >
-              <Typography variant="h6" fontWeight={"bold"}>
-                {productCart.productReference.name}
-              </Typography>
-              <Typography variant="body1">Location à la journée</Typography>
-              <Typography variant="body1">
-                Quantité: {productCart.quantity}
-              </Typography>
-              <Typography variant="body1">
-                Période: {format(productCart.dateTimeStart, "dd/MM/yyyy")} -{" "}
-                {format(productCart.dateTimeEnd, "dd/MM/yyyy")}
-              </Typography>
-              <Typography variant="body1">
-                Prix: {productCart.productReference.price} €
-              </Typography>
+              <Box sx={{ flex: 1 }}>
+                <Typography variant="h6" fontWeight={"bold"}>
+                  {productCart.productReference.name}
+                </Typography>
+                <Typography variant="body1">Location à la journée</Typography>
+                <Typography variant="body1">
+                  Quantité: {productCart.quantity}
+                </Typography>
+                <Typography variant="body1">
+                  Période: {format(productCart.dateTimeStart, "dd/MM/yyyy")} -{" "}
+                  {format(productCart.dateTimeEnd, "dd/MM/yyyy")}
+                </Typography>
+                <Typography variant="body1">
+                  Prix: {(productCart.productReference.price / 100).toFixed(2)}{" "}
+                  € TTC/jour
+                </Typography>
+              </Box>
+              <ProductCartAvailability productCart={productCart} />
             </Box>
           );
         })}
---
+++ src/components/cart/OrderCard.tsx
@@ -0,0 +1,117 @@
+import { MUTATION_ORDER } from "@/graphql/order/order";
+import { OrangeBtnWhiteHover } from "@/styles/MuiButtons";
+import { useMutation } from "@apollo/client";
+import {
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { showToast } from "../utils/toastHelper";
+import { useRouter } from "next/router";
+
+function OrderCard({ totalPrice }: { totalPrice: number }) {
+  const router = useRouter();
+  const totalPriceString = (totalPrice / 100).toFixed(2);
+  const TVA = (Number(totalPriceString) * (20 / 100)).toFixed(2);
+
+  const [doOrder] = useMutation(MUTATION_ORDER);
+
+  const handleOrder: React.MouseEventHandler<HTMLButtonElement> = async () => {
+    try {
+      await doOrder();
+      showToast("success", "Produit ajouté au panier");
+      router.replace("/");
+    } catch (error) {
+      showToast("error", "Une erreur s'est produite");
+    }
+  };
+  return (
+    <Card
+      sx={{
+        width: "100%", // Take full width of the container
+        maxWidth: 600,
+        borderRadius: "10px",
+        mx: "auto",
+        my: 4,
+        boxShadow: "none", // Remove shadow
+        backgroundColor: "white", // Set background to white
+        px: 2, // Add horizontal padding
+      }}
+    >
+      <CardContent>
+        <Stack spacing={2}>
+          {/* Row 1 */}
+          <Grid container alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body2" fontSize={"small"}>
+                État de la demande :
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Box
+                sx={{
+                  padding: "2px 10px",
+                  fontSize: "small",
+                  backgroundColor: "rgba(0, 255, 0, 0.4)",
+                  borderRadius: "10px",
+                  display: "inline-block",
+                }}
+              >
+                <Typography variant="body2" fontSize={"small"}>
+                  disponible
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          <Divider />
+
+          {/* Row 2 */}
+          <Grid container alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1">TVA 20%</Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant="body1">{TVA} €</Typography>
+            </Grid>
+          </Grid>
+          <Divider />
+
+          {/* Row 3 */}
+          <Grid container alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1">Article HT</Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant="body1">---</Typography>
+            </Grid>
+          </Grid>
+          <Divider />
+
+          {/* Row 4 */}
+          <Grid container alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1">Total</Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant="body1">{totalPriceString} € TTC</Typography>
+            </Grid>
+          </Grid>
+          <Divider />
+
+          {/* Button */}
+          <Box display="flex" justifyContent="flex-end" mt={2}>
+            <OrangeBtnWhiteHover onClick={handleOrder}>
+              Passer au paiement
+            </OrangeBtnWhiteHover>
+          </Box>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default OrderCard;
